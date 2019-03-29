2311f11c7cc7e1cd0348d81e7e953530c932ab0c
AcheZeta <ache.mariana@gmail.com>
2019-03-28 19:39:38 -0600

    Lee el readme en la consola

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 module.exports = () => {
   // ...
 };
+
+//Nombramos variable con el nombre del modulo que queremos importar
+let fs = require('fs');
+//Variable con el nombre del archivo
+let document = ('./README.md')
+//Le indicamos el modulo y la acción con tres parámetros.
+//1o archivo que deseamos leer.
+//2o parametro opcional (carácteres)
+//3o Callback con dos parametros, error y el archivo
+fs.readFile(document, 'utf-8', (err, data) => {
+    if (err) {
+        console.log('error: ', err);
+    } else {
+        console.log(data);
+    }
+});
+
+
