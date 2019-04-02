9a746238bd12d12873b9929ee651e0626982d326
AcheZeta <ache.mariana@gmail.com>
2019-04-02 12:55:25 -0600

    Toma la variable docname desde la consola

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ module.exports = () => {
   // ...
 };
 
-//Nombramos variable con el nombre del modulo que queremos importar
+// Nombramos variable con el nombre del modulo que queremos importar
 let fs = require('fs');
-//Variable con el nombre del archivo
-let document = ('./README.md')
-//Le indicamos el modulo y la acción con tres parámetros.
-//1o archivo que deseamos leer.
-//2o parametro opcional (carácteres)
-//3o Callback con dos parametros, error y el archivo
-fs.readFile(document, 'utf-8', (err, data) => {
+let path = require('path');
+
+
+console.log(process.argv);
+ function grab(flag) {
+     var index = process.argv.indexOf(flag);
+        return(index === -1) ? null : process.argv[index+1];
+ }
+  var docname = grab('--doc');
+  
+  if (!user || !docname) {
+      console.log("Falta algo")
+  } else {
+      console.log(`${docname} ${user}`)
+  }
+
+//Le indicamos el modulo y la acción con tres parámetros. 1o archivo que deseamos leer. 2o parametro opcional (carácteres), 3o Callback con dos parametros, error y el archivo
+fs.readFile(docname, 'utf-8', (err, data) => {
     if (err) {
         console.log('error: ', err);
     } else {
@@ -18,4 +29,17 @@ fs.readFile(document, 'utf-8', (err, data) => {
     }
 });
 
+// //Crear Variables para que ingresa el usuario. 
+// //Argumento2
+// let file = process.argv[2]
+// //Ruta absoluta
+// let pathAb = file.path.resolve
+// //extension del archivo. 
+// let ext = file.path.extname
+ 
+
+// console.log(path)
+
+//Variable con el nombre del archivo
+// let document = `${docname}`
 
