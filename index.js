09de6c1bf2d575962a22d85c34d94f70ceee56dc
AcheZeta <ache.mariana@gmail.com>
2019-04-08 16:06:38 -0500

    Intento CLI

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-// Nombramos variable con el nombre del modulo que queremos importar
+'use stric';
+//Importa los modulos que vamos a ocupar  
 let fs = require('fs');
 let path = require('path');
 const fetch = require('node-fetch')
 
-console.log(process.argv);
 
-function grab(flag) {
-  var index = process.argv.indexOf(flag);
-  return (index === -1) ? null : process.argv[index + 1];
-}
-let docname = grab('--doc');
+ console.log(process.argv);
 
-if (!docname) {
-  console.log("Especifica el nombre del archivo, usa --doc")
-} else {
-  readFiles()
-}
+// function grab(flag) {
+//   var index = process.argv.indexOf(flag);
+//   return (index === -1) ? null : process.argv[index + 1];
+// }
+// let docname = grab('--doc');
 
-//Convertir el Path 
-let filePath = docname
-let pathResolve = path.resolve(filePath)
-let pathNormalize = path.normalize(pathResolve)
-// let pathExtName = path.extname(pathNormalize)
+// if (!docname) {
+//   console.log("Especifica el nombre del archivo, usa --doc")
+// } else {
+//   readFiles()
+// }
 
-const validate = (docname) => {
-  docname.forEach(element => {
-    fetch(res => element)
-      .then(res => {
-        const linkStatus = `Element ${element} status${res.status} text${res.statusText}`
-        console.log(linkStatus)
-      })
-      .catch(err => console.log(err))
-  });
-}
+// //Convertir el Path 
+// let filePath = docname
+// let pathResolve = path.resolve(filePath)
+// let pathNormalize = path.normalize(pathResolve)
+// // let pathExtName = path.extname(pathNormalize)
 
-console.log(validate)
-//   )
+// const validate = (docname) => {
+//   docname.forEach(element => {
+//     fetch(res => element)
+//       .then(res => {
+//         const linkStatus = `Element ${element} status${res.status} text${res.statusText}`
+//         console.log(linkStatus)
+//       })
+//       .catch(err => console.log(err))
+//   });
+// }
 
-//Le indicamos el modulo y la acción con tres parámetros. 1o archivo que deseamos leer. 2o parametro opcional (carácteres), 3o Callback con dos parametros, error y el archivo
-function readFiles() {
-  fs.readFile(docname, 'utf-8', (err, data) => {
-    if (err) {
-      console.log('error: ', err);
-    } else {
-      const regExp = /(https?:\/\/[^\s]+)/g;
-      const result = data.match(regExp);
-      console.log(data);
-      console.log(`links: ${result}`)
-    }
-  });
-}
+// console.log(validate)
+// //   )
+
+// //Le indicamos el modulo y la acción con tres parámetros. 1o archivo que deseamos leer. 2o parametro opcional (carácteres), 3o Callback con dos parametros, error y el archivo
+// function readFiles() {
+//   fs.readFile(docname, 'utf-8', (err, data) => {
+//     if (err) {
+//       console.log('error: ', err);
+//     } else {
+//       const regExp = /(https?:\/\/[^\s]+)/g;
+//       const result = data.match(regExp);
+//       console.log(data);
+//       console.log(`links: ${result}`)
+//     }
+//   });
+// }
